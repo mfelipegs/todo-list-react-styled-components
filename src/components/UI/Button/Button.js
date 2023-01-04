5b5ba040696a0c87be4c91bd7467b73002bfb35b
mfelipegs <matheusg.dasilva7@gmail.com>
2023-01-04 19:56:06 -0300

    Feat: CSS Modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,9 @@
-import styled from "styled-components";
+import React from "react";
 
-const Button = styled.button`
+import styles from './Button.module.css';
+//import styled from "styled-components";
+
+/*const Button = styled.button`
     width: 100%;
     font: inherit;
     padding: 0.5rem 1.5rem;
@@ -18,20 +21,28 @@ const Button = styled.button`
     outline: none;
   }
 
+  &.invalid input {
+    border-color: red;
+    background: #ffd7d7;
+  }
+
+  &.invalid label {
+    color: red;
+  }
   &:hover,
   &:active {
     background: #ac0e77;
     border-color: #ac0e77;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
-`;
+`; */
 
-//const Button = props => {
-//  return (
-//    <button type={props.type} className="button" onClick={props.onClick}>
-//      {props.children}
-//    </button>
-//  );
-//};
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
